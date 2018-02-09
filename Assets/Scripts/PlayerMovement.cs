6f1bb586400811bf0b4e8d43bb4e8ffec7ddfd11
Tom Sauer <tom@exluto.com>
2018-02-08 19:50:00 -0500

    adding player movement, added skybox from asset store, added grid, working on spawning wall peices

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+	public KeyCode upKey;
+	public KeyCode downKey;
+	public KeyCode rightKey;
+	public KeyCode leftKey;
+	private float m_speed = 16;
+	public Rigidbody2D m_rb;
+	public GameObject m_wallprefab;
+	Collider2D m_wall;
+	void Start () {
+		m_rb=GetComponent<Rigidbody2D>();
+	}
+	void Wall() {
+		GameObject g = (GameObject)Instantiate(m_wallprefab, transform.position, Quaternion.identity);
+		m_wall = GetComponent<Collider2D>();
+	}
+	void Update () {
+		if(Input.GetKeyDown(downKey)) {
+			m_rb.velocity = Vector2.down * m_speed;
+
+		} else if (Input.GetKeyDown(upKey)) {
+			m_rb.velocity = Vector2.up * m_speed;
+
+		} else if (Input.GetKeyDown(rightKey)) {
+			m_rb.velocity = Vector2.right * m_speed;
+
+		} else if (Input.GetKeyDown(leftKey)) {
+			m_rb.velocity = Vector2.left * m_speed;
+
+		}
+		
+	}
+}
