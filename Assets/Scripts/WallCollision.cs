997599431b8241d3a68753b695d1d9e6b4d1d02a
Tom Sauer <tom@exluto.com>
2018-07-08 20:35:30 -0400

    fixing wall collision for second player

--- Assets/Scripts/WallCollision.cs
+++ Assets/Scripts/WallCollision.cs
@@ -7,6 +7,9 @@ public class WallCollision : MonoBehaviour {
 	void OnCollisionEnter(Collision collision) {
 		if(collision.transform.tag == "Player") {
 			Destroy(collision.gameObject);
+
+		} else if(collision.transform.tag == "Player1") {
+			Destroy(collision.gameObject);
 		}
 	}
 }
